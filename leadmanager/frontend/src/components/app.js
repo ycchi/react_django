264c5d1d58c32fd94c6adbaa33a89efec59e2ecc
ycchi <44580684+ycchi@users.noreply.github.com>
2020-09-02 16:59:17 -0400

    finished 2

---
+++ leadmanager/frontend/src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
